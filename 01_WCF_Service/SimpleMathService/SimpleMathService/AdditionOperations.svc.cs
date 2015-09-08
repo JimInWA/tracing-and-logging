03ac981823d1ff2a5be81fa78da4073352fc3c68
JimInWA <jim.a.fisher.biz@gmail.com>
2015-09-08 07:13:19 -0700

    Adding the WCF Service and the initial proxy class (service class) that was generated via svcutil.exe

---
+++ 01_WCF_Service/SimpleMathService/SimpleMathService/AdditionOperations.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SimpleMathService
+{
+    public class Service1 : IAdditionOperations
+    {
+        public int AddTwoNumbers(int valueA, int valueB)
+        {
+            // simple error that could have been caught with a unit test, here just for the proof of concept
+            var result = valueA + valueB - 1;
+            return result;
+        }
+    }
+}
