bace23b8fe1ce7c5022fdd13faa25461f50df966
JimInWA <jim.a.fisher.biz@gmail.com>
2015-09-27 20:18:49 -0700

    Adding code using IClientMessageInspector which is based on http://westdiscgolf.blogspot.ie/2012/09/debugging-wcf-messages-before.html and https://github.com/WestDiscGolf/WcfMessageDebugging.
    Note: have not changed the application to use this code yet.

---
+++ 04_ClientApplication_b/SimpleMathClient/SoapRequestAndResponseTracing/DebugMessageDispatcher.cs
@@ -0,0 +1,31 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+
+namespace SoapRequestAndResponseTracing
+{
+    public class DebugMessageDispatcher : IDispatchMessageInspector
+    {
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            var messageBuffer = request.CreateBufferedCopy(int.MaxValue);
+
+            var messageReader = new MessageReader();
+            messageReader.Read(messageBuffer);
+
+            request = messageBuffer.CreateMessage();
+            return request;
+        }
+
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            // var messageBuffer = reply.CreateBufferedCopy(int.MaxValue);
+
+            //var messageReader = new MessageReader();
+            //messageReadermr.Read(messageBuffer);
+
+            //reply = messageBuffer.CreateMessage();
+        }
+    }
+
+}
---
+++ 04_ClientApplication_b/SimpleMathClient/SoapRequestAndResponseTracing/MessageReader.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.ServiceModel.Channels;
+using System.Xml;
+
+namespace SoapRequestAndResponseTracing
+{
+    public class MessageReader
+    {
+        public void Read(MessageBuffer messageBuffer)
+        {
+            var memoryStream = new MemoryStream();
+            messageBuffer.WriteMessage(memoryStream);
+            memoryStream.Position = 0;
+            var xmlDoc = new XmlDocument();
+            xmlDoc.PreserveWhitespace = true;
+            xmlDoc.Load(memoryStream);
+        }
+    }
+}
