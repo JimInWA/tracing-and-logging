32499ec3c39b3cb6c9753dc2b162b6b05d05b0ef
JimInWA <jim.a.fisher.biz@gmail.com>
2015-11-02 04:32:57 -0800

    Reorganizing to a more flattened folder structure

---
+++ 01_WCF_Service/SimpleMathService/IAdditionOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SimpleMathService
+{
+    [ServiceContract]
+    public interface IAdditionOperations
+    {
+
+        [OperationContract]
+        int AddTwoNumbers(int valueA, int valueB);
+    }
+}
---
+++ 02_ClientApplication/SimpleMathClient/ViewModels/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleMathClient.ViewModels
+{
+    public class HomeViewModel
+    {
+        public int InputA { get; private set; }
+        public int InputB { get; private set; }
+        public int Result { get; private set; }
+
+        public void AddTwoNumbers(int inputA, int inputB)
+        {
+            InputA = inputA;
+            InputB = inputB;
+
+            var additioinOperationProxy = new AdditionOperationsClient();
+
+            Result = additioinOperationProxy.AddTwoNumbers(inputA, inputB);
+        }
+    }
+}
